fce1b49181946d8840f0db99266013443015c012
Allngh <naghavi1381@gmail.com>
2022-07-06 12:33:44 +0430

    add some methods for easier managing main method

--- src/Customer.java
+++ src/Customer.java
@@ -1,11 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Customer extends Person{
     boolean rentOrOwn;//true for own and false for rent
-    int debt = 0;
     House house;
-    List<Payment> paymentsHistory = new ArrayList<>();
+
+    public void changeSitRequest(Situation situation) throws InvalidRequestException{
+        if(house.situation.equals(Situation.rent)){
+            if(situation.equals(Situation.rent)){
+                throw new InvalidRequestException("you cant rent a house that has been rent");
+            }
+        }else if(house.situation.equals(Situation.bought)){
+            if(situation.equals(Situation.rent)){
+                throw new InvalidRequestException("you cant rent your house");
+            }
+        }
+        //request to manager needs
+    }
 
     public Customer(boolean rentOrOwn,String name,String lastName,House house) {
         this.rentOrOwn = rentOrOwn;
@@ -15,11 +27,20 @@ public class Customer extends Person{
     }
 
     public List<Payment> timedPayments(){
-        return paymentsHistory.stream().sorted((a1,a2) -> (a1.date.compare(a2.date))).toList();
+        return house.paymentsHistory.stream().sorted((a1,a2) -> (a1.date.compare(a2.date))).toList();
     }
 
     public List<Payment> filteredPaymentList(PaymentReson paymentReson){
-        return paymentsHistory.stream().filter(p -> p.paymentReson.equals(paymentReson)).toList();
+        return house.paymentsHistory.stream().filter(p -> p.paymentReson.equals(paymentReson)).toList();
+    }
+
+    public List<Payment> timeFilterPaymentList(Date date1,Date date2){
+        return house.paymentsHistory.stream().filter(p -> (p.date.compare(date1) > 0)&&(p.date.compare(date2) < 0)).
+                sorted((a1,a2) -> (a1.date.compare(a2.date))).toList();
+    }
+
+    public Map<Integer,PaymentReson> debts(){
+        return house.debtReson;
     }
 
 }
--- src/House.java
+++ src/House.java
@@ -1,20 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class House {
-    private int ID;
-    private int floor;
-    private int metrage;
+    final int ID;
+    final int floor;
+    final int meterage;
+    final int numOfRooms;int numOfUser;
+
     List<PaymentReson> paymentResonList;
+    Map<Integer,PaymentReson> debtReson = new HashMap<>();
+    List<Payment> paymentsHistory = new ArrayList<>();
     Situation situation;
 
     public void changeSituation(Situation situation){
         this.situation = situation;
     }
 
-    public House(int ID, int floor, int metrage) {
+    public House(int ID, int floor, int meterage, int numOfRooms, int numOfUser) {
         this.ID = ID;
         this.floor = floor;
-        this.metrage = metrage;
+        this.meterage = meterage;
+        this.numOfRooms = numOfRooms;
+        this.numOfUser = numOfUser;
     }
 }
 
